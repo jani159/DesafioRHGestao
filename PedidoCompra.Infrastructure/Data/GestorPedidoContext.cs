7332a15b52791de53a279e8d451267e6950e5f27
jani159 <janiel_amorim@hotmail.com>
2025-06-16 00:08:59 -0300

    feat(infrastructure): adiciona DbContext
    - Criado GestorPedidosContext com DbSet para Pedido, Produto, Cliente e PedidoItem.
    - Adicionada configuração de conexão no appsettings.json.
    - Registrado contexto no Program.cs com suporte a PostgreSQL.

---
+++ PedidoCompra.Infrastructure/Data/GestorPedidoContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestorPedidos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorPedidos.Infrastructure.Data
+{
+    public class GestorPedidoContext : DbContext
+    {
+        public GestorPedidoContext(DbContextOptions<GestorPedidoContext> options)
+            : base(options)
+        {
+        }
+        // DbSets
+         public DbSet<Pedido> Pedidos { get; set; }
+         public DbSet<ItemPedido> ItensPedido { get; set; }
+         public DbSet<Pedido> Cliente { get; set; }
+         public DbSet<Pedido> Produto { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(GestorPedidoContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
